60a0072b6edf4a7a27d9fc81cd9578b562a84243
romaobernnardo <romaobernnardo>
2023-10-23 15:38:45 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+	return res.json('Api está Ok!')
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+	console.log(`Servidor em pé na porta ${port}`)
+})
